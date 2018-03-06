514100306dfb1499db48a2cadc20d4323ab6a703
mzrieker <mzrieker@gmail.com>
2018-03-06 16:02:03 -0700

    Added Socket Test Client App

--- SocketTest/SocketTest/SocketTest/MainPage.xaml.cs
+++ SocketTest/SocketTest/SocketTest/MainPage.xaml.cs
@@ -16,26 +16,21 @@ using Windows.UI.Xaml.Navigation;
 using SeniorDesign;
 
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace SocketTest
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         public MainPage()
         {
             this.InitializeComponent();
         }
+
         static string PortNumber = "1337";
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             this.StartServer();
-            this.StartClient();
-            
+            //this.StartClient();
         }
 
         private async void StartServer()
@@ -47,7 +42,8 @@ namespace SocketTest
                 // The ConnectionReceived event is raised when connections are received.
                 streamSocketListener.ConnectionReceived += this.StreamSocketListener_ConnectionReceived;
 
-                // Start listening for incoming TCP connections on the specified port. You can specify any port that's not currently in use.
+                // Start listening for incoming TCP connections on the specified port. 
+                // You can specify any port that's not currently in use. Here I chose port 1337.
                 await streamSocketListener.BindServiceNameAsync(MainPage.PortNumber);
 
                 this.serverListBox.Items.Add("server is listening...");
@@ -84,57 +80,6 @@ namespace SocketTest
             sender.Dispose();
 
             await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () => this.serverListBox.Items.Add("server closed its socket"));
-        }
-
-        private async void StartClient()
-        {
-            try
-            {
-                // Create the StreamSocket and establish a connection to the echo server.
-                using (var streamSocket = new Windows.Networking.Sockets.StreamSocket())
-                {
-                    // The server hostname that we will be establishing a connection to. In this example, the server and client are in the same process.
-                    var hostName = new Windows.Networking.HostName("localhost");
-
-                    this.clientListBox.Items.Add("client is trying to connect...");
-
-                    await streamSocket.ConnectAsync(hostName, MainPage.PortNumber);
-
-                    this.clientListBox.Items.Add("client connected");
-
-                    // Send a request to the echo server.
-                    string request = "Hello, World!";
-                    using (Stream outputStream = streamSocket.OutputStream.AsStreamForWrite())
-                    {
-                        using (var streamWriter = new StreamWriter(outputStream))
-                        {
-                            await streamWriter.WriteLineAsync(request);
-                            await streamWriter.FlushAsync();
-                        }
-                    }
-
-                    this.clientListBox.Items.Add(string.Format("client sent the request: \"{0}\"", request));
-
-                    // Read data from the echo server.
-                    string response;
-                    using (Stream inputStream = streamSocket.InputStream.AsStreamForRead())
-                    {
-                        using (StreamReader streamReader = new StreamReader(inputStream))
-                        {
-                            response = await streamReader.ReadLineAsync();
-                        }
-                    }
-
-                    this.clientListBox.Items.Add(string.Format("client received the response: \"{0}\" ", response));
-                }
-
-                this.clientListBox.Items.Add("client closed its socket");
-            }
-            catch (Exception ex)
-            {
-                Windows.Networking.Sockets.SocketErrorStatus webErrorStatus = Windows.Networking.Sockets.SocketError.GetStatus(ex.GetBaseException().HResult);
-                this.clientListBox.Items.Add(webErrorStatus.ToString() != "Unknown" ? webErrorStatus.ToString() : ex.Message);
-            }
         }
     }
 }
---
+++ SocketTest_Client/SocketTest_Client/SocketTest_Client/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace SocketTest_Client
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        static string PortNumber = "1337";
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            //this.StartServer();
+            this.StartClient();
+        }
+
+        private async void StartClient()
+        {
+            try
+            {
+                // Create the StreamSocket and establish a connection to the echo server.
+                using (var streamSocket = new Windows.Networking.Sockets.StreamSocket())
+                {
+                    // The server hostname that we will be establishing a connection to.
+                    // In this example, the server and client are in the same process.
+                    var hostName = new Windows.Networking.HostName("localhost");
+
+                    this.clientListBox.Items.Add("client is trying to connect...");
+
+                    await streamSocket.ConnectAsync(hostName, MainPage.PortNumber);
+
+                    this.clientListBox.Items.Add("client connected");
+
+                    // Send a request to the echo server.
+                    string request = "Hello, World!";
+                    using (Stream outputStream = streamSocket.OutputStream.AsStreamForWrite())
+                    {
+                        using (var streamWriter = new StreamWriter(outputStream))
+                        {
+                            await streamWriter.WriteLineAsync(request);
+                            await streamWriter.FlushAsync();
+                        }
+                    }
+
+                    this.clientListBox.Items.Add(string.Format("client sent the request: \"{0}\"", request));
+
+                    // Read data from the echo server.
+                    string response;
+                    using (Stream inputStream = streamSocket.InputStream.AsStreamForRead())
+                    {
+                        using (StreamReader streamReader = new StreamReader(inputStream))
+                        {
+                            response = await streamReader.ReadLineAsync();
+                        }
+                    }
+
+                    this.clientListBox.Items.Add(string.Format("client received the response: \"{0}\" ", response));
+                }
+
+                this.clientListBox.Items.Add("client closed its socket");
+            }
+            catch (Exception ex)
+            {
+                Windows.Networking.Sockets.SocketErrorStatus webErrorStatus = Windows.Networking.Sockets.SocketError.GetStatus(ex.GetBaseException().HResult);
+                this.clientListBox.Items.Add(webErrorStatus.ToString() != "Unknown" ? webErrorStatus.ToString() : ex.Message);
+            }
+        }
+    }
+}
